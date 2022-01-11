053b6b5bdd714d7431c47602252ebae0be9b61d7
Manoj Krish <93692786+manojkrishkumar@users.noreply.github.com>
2022-01-11 11:13:32 +0530

    Update final.sh

--- final.sh
+++ final.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 cd /tmp/rom
 
-export CCACHE_DIR=/tmp/ccache
-export CCACHE_EXEC=$(which ccache)
-export USE_CCACHE=1
-ccache -M 25G
-ccache -o compression=true
-ccache -z
+#export CCACHE_DIR=/tmp/ccache
+#export CCACHE_EXEC=$(which ccache)
+#export USE_CCACHE=1
+#ccache -M 25G
+#ccache -o compression=true
+#ccache -z
 
 . build/envsetup.sh
 lunch evolution_laurel_sprout-userdebug
